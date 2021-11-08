b42746e1cd810286475c512bccb3fd10731197be
clearsky77 <clearsky3264@gmail.com>
2021-11-08 10:50:44 +0900

    로딩화면 추가

---
+++ app/src/main/java/com/clearsky77/librarypractice_20211107/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.clearsky77.librarypractice_20211107
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setupEvents()
+        setValues()
+    }
+
+    private fun setValues() {
+    }
+
+    private fun setupEvents() {
+    }
+}
